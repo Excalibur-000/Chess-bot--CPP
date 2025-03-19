d4696009ec5bb46387492a5b5d22adec942d9805
Excalibur-000 <arthurgiannios@outlook.com>
2025-03-19 11:21:15 +1000

    feat: further annotation

--- src/bot.cpp
+++ src/bot.cpp
@@ -11,8 +11,9 @@
 
 
 //! Generate pseudo-legal moves for a given position
+//* Each functions takes in the piece and the starting square of that piece
+
 //? Sliding pieces (Bishop, Rook, Queen)
-//* Take in the piece and the starting square of that piece
 // Loop over 8 different directions, and loop over number of squares to the edge in that direction (Using prepared function from earlier)
 // Use direction offsets to check this
 // Skip further if there is a friendly piece in the way
@@ -20,6 +21,8 @@
 // Add logic so that Bishop checks the last 4 offsets (Horizontal), and Rook checks first 4 (straight). Queen can use all 8. 
 
 //? King
+// Uses direction offsets to check all adjacent squares 
+// Uses boolean state to determine if king can castle
 
 //? Pawns
 
